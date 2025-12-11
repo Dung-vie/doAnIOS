e8f50b279a6eb7a3da0cfb7fa8a3b62063e5bbcc
 User <user@MacBook-Pro.local>
2025-12-11 14:01:45 +0300

    update database

---
+++ hocTap/FMDB/Quiz_Bridging_Header_h.h
@@ -0,0 +1,14 @@
+//
+//  Quiz_Bridging_Header_h.h
+//  hocTap
+//
+//  Created by  User on 10.12.2025.
+//
+
+#ifndef Quiz_Bridging_Header_h_h
+#define Quiz_Bridging_Header_h_h
+
+
+#endif /* Quiz_Bridging_Header_h_h */
+
+#import "FMDB.h"
---
+++ hocTap/FMDB/hocTap-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  hocTap-Bridging-Header.h
+//  hocTap
+//
+//  Created by  User on 10.12.2025.
+//
+
+#import "FMDB.h"	

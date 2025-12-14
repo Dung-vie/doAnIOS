72242b64d5380862e668d6f622217cce258a8a91
Dung-vie <dunghoang1122004@gmail.com>
2025-12-14 16:02:49 +0300

    tao folder mvc

---
+++ appHocTap/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  appHocTap
+//
+//  Created by  User on 12.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("may GAY")
+        }
+        .padding()
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ appHocTap/appHocTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  appHocTapApp.swift
+//  appHocTap
+//
+//  Created by  User on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct appHocTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
